5181a6b918978a229d9cbeda3b8835bcc75a96e1
wayned27 <wayned27@outlook.com>
2025-04-25 10:37:28 -0400

    Final db changes

--- database/insert_users.py
+++ database/insert_users.py
@@ -1,4 +1,5 @@
 from user_db import add_user
 
-add_user("Wayne's White Card", "F3 7D 28 13", temp_threshold=22, light_threshold=600)
-add_user("Wayne's Blue Tag", "33 A1 68 0D", temp_threshold=22, light_threshold=600)
+add_user("Kamran's Blue Tag", "53 F6 E6 F7", temp_threshold=22, light_threshold=1500)
+add_user("Wayne's Blue Tag", "33 A1 68 0D", temp_threshold=23, light_threshold=1700)
+add_user("Nathaniel's Blue Tag", "23 8C B7 CD", temp_threshold=24, light_threshold=1600)
